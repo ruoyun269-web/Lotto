fd996678604ce95a2e232ba829a099135749c83f
鍾若筠 <ruoyun269@gmail.com>
2025-09-15 17:05:38 +0800

    第六次commit (按鈕壓下去數字會動)

--- app/src/main/java/tw/edu/pu/csim/s1132253/lotto/MainActivity.kt
+++ app/src/main/java/tw/edu/pu/csim/s1132253/lotto/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -11,9 +12,10 @@ import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import tw.edu.pu.csim.s1132253.lotto.ui.theme.LottoTheme
 
 class MainActivity : ComponentActivity() {
@@ -32,20 +34,18 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun play(modifier: Modifier = Modifier) {
-    var lucky = (1..100).random()
+    // 使用 remember 和 mutableStateOf 儲存樂透數字狀態
+    val lucky = remember { mutableStateOf((1..100).random()) }
 
     Column(
-        modifier = modifier.fillMaxSize(), // 讓 Column 填滿整個畫面
-        horizontalAlignment = Alignment.CenterHorizontally, // 水平置中
-        verticalArrangement = androidx.compose.foundation.layout.Arrangement.Center // 垂直置中
+        modifier = modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
-        Text(
-            text = "樂透數字(1-100)為 $lucky",
-        )
-        Button(
-            onClick = { lucky = (1..100).random() }
-        ) {
+        Text(text = "樂透數字(1-100)為 ${lucky.value}")
+        Button(onClick = { lucky.value = (1..100).random() }) {
             Text("重新產生樂透碼")
         }
     }
 }
+
